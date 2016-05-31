df6b5812f81a5c9486c381954f068a82dac3a5f1
yoichi kobayashi <info@tplh.net>
2016-05-31 13:14:19 +0900

    [add] Cover Class was added, and rename canvas id for Background Class.

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,16 @@
+import Background from './modules/background.js';
+import Cover from './modules/cover.js';
+
 import initIndex from './init/index.js'
 
 const { pathname } = window.location;
+const background = new Background(document.getElementById('canvas-background'));
+const cover = new Cover(document.getElementById('canvas-cover'));
+
+const render = () => {
+  background.render();
+  cover.render();
+}
 
 const init = () => {
   switch (pathname.replace('index.html', '')) {
@@ -12,10 +22,11 @@ const init = () => {
 }
 init();
 
+// ************
+// For Google Analytics
 (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
 (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
 m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
 ga('create', 'UA-568033-1', 'auto');
 ga('send', 'pageview');
--- src/js/modules/cover.js
+++ src/js/modules/cover.js
@@ -1,10 +1,11 @@
 import THREE from ('three');
 import glslify from ('glslify');
 
-export default class Background {
+export default class Cover {
   constructor(canvas) {
     this.renderer = new THREE.WebGLRenderer({
       canvas: canvas,
+      alpha: true,
       antialias: true,
     });
   }
