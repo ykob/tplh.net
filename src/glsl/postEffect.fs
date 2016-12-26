40b255f53d7693db0b45fabe5af9caef803d9d61
yoichi kobayashi <info@tplh.net>
2016-12-27 01:33:16 +0900

    [add] externalize glsl random function.

--- src/glsl/postEffect.fs
+++ src/glsl/postEffect.fs
@@ -7,12 +7,10 @@ uniform sampler2D texture;
 varying vec3 vPosition;
 varying vec2 vUv;
 
-float rand(vec2 co){
-  return fract(sin(dot(co.xy ,vec2(12.9898,78.233))) * 43758.5453);
-}
+#pragma glslify: random = require(glsl-util/random);
 
 void main() {
-  float noise = rand(vPosition.xy + time);
+  float noise = random(vPosition.xy + time);
   vec4 color = texture2D(texture, vUv);
   gl_FragColor = color + vec4(vec3(noise * 0.1), 1.0);
 }
